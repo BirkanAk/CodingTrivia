0c07d8172c069abc8c8c2cab03de70bccb8959ff
BirkanAk <bak1@binghamton.edu>
2021-04-16 17:50:06 +0300

    Main Menu View done. A lot to do

--- app/src/main/java/com/example/codingtrivia/MainActivity.java
+++ app/src/main/java/com/example/codingtrivia/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.codingtrivia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openQuickPlay(View view) {
+    }
+
+    public void onSettings(View view) {
+    }
+
+    public void onTimeTrial(View view) {
+    }
+
+    public void onAdvancedClick(View view) {
     }
 }
