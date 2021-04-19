1ebf65ac4fd642713bab20e49aa7cd4f81e08a21
BirkanAk <bak1@binghamton.edu>
2021-04-19 17:23:38 +0300

    Go Back button to be implemented, prototype in Time-Trials.

--- app/src/main/java/com/example/codingtrivia/TimeTrials.java
+++ app/src/main/java/com/example/codingtrivia/TimeTrials.java
@@ -2,7 +2,9 @@ package com.example.codingtrivia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TimeTrials extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class TimeTrials extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_trials);
+    }
+
+    public void onGoBackClick(View view) {
+        Intent act_action = new Intent(this, MainActivity.class);
+        startActivity(act_action);
     }
 }
