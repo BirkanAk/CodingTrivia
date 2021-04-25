8b0d19956620644ef857bc3cbc0ebe80b423d9e6
BirkanAk <bak1@binghamton.edu>
2021-04-26 00:44:13 +0300

    Ready to be tested from the text in QuickPlay

--- app/src/main/java/com/example/codingtrivia/QuickPlay.java
+++ app/src/main/java/com/example/codingtrivia/QuickPlay.java
@@ -5,14 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class QuickPlay extends AppCompatActivity {
+    TextView question_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quick_play);
+
+        question_text=(TextView)findViewById(R.id.question_text);
+        QuestionsArray.getInstance();
+        //
     }
+
+
 
     public void onGoBackClick(View view) {
         Intent act_action = new Intent(this, MainActivity.class);
