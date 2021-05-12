3ab8587eb5b9ff5d9986ae9162c98c7fd40b845b
gurseskerem <keremgurses1997@gmail.com>
2021-05-12 17:26:23 +0300

    Adding Binghamton Questions.

--- app/src/main/java/com/example/codingtrivia/QuestionsArray.java
+++ app/src/main/java/com/example/codingtrivia/QuestionsArray.java
@@ -5,13 +5,14 @@ public class QuestionsArray {
     public static Questions[] java_questions;
     public static Questions[] python_questions;
     public static Questions[] cpp_questions;
-    public static Questions[] advanced_questions;
+    public static Questions[] bing_questions;
     public static String selected;
 
     public QuestionsArray(){
         java_questions=new Questions[5];
         python_questions=new Questions[5];
         cpp_questions=new Questions[5];
+        bing_questions=new Questions[10];
 
         Questions q;
 
@@ -31,34 +32,64 @@ public class QuestionsArray {
         python_questions[4]=q;
 
         q=new Questions(QType.Cpp,"Choose the operator which cannot be overloaded.","/","()","::","::");
-        cpp_questions[5]=q;
+        cpp_questions[0]=q;
 
         q=new Questions(QType.Cpp,"A user defined header file is included by following statement in general.","#include “file.h\""," #include <file.h>","#include <file>","#include “file.h\"");
-        cpp_questions[6]=q;
+        cpp_questions[1]=q;
 
         q=new Questions(QType.Cpp,"Class function which is called automatically as soon as the object is created is called as __","Constructor","Destructor","Friend function","Constructor");
-        cpp_questions[7]=q;
+        cpp_questions[2]=q;
 
         q=new Questions(QType.Cpp,"Objects created using new operator are stored in __ memory.","Cache","Heap","Stack","Heap");
-        cpp_questions[8]=q;
+        cpp_questions[3]=q;
 
         q=new Questions(QType.Cpp,"A C++ program statements can be commented using","Single line comment","Multi line comment","Both (a) and (b).","Both (a) and (b).");
-        cpp_questions[9]=q;
+        cpp_questions[4]=q;
 
         q=new Questions(QType.Java,"What is the default value of byte variable?","0","0.0","null","0");
-        java_questions[10]=q;
+        java_questions[0]=q;
 
         q=new Questions(QType.Java,"java.util.Collections is a:","Class","Interface","Object","Class");
-        java_questions[11]=q;
+        java_questions[1]=q;
 
         q=new Questions(QType.Java,"Which of those allows duplicate elements?","Set","List","All","List");
-        java_questions[12]=q;
+        java_questions[2]=q;
 
         q=new Questions(QType.Java,"Which of those doesn’t have an index based structure?","List","Set","Map","Set");
-        java_questions[13]=q;
+        java_questions[3]=q;
 
         q=new Questions(QType.Java,"Which allows the storage of a null key and null values?","Hashtable","HashMap","None of the above","HashMap");
-        java_questions[14]=q;
+        java_questions[4]=q;
+
+        q=new Questions(QType.Java,"Which company was founded in Endicott and maintained its R&D campus there until the early 1990s?","IBM","Apple","Amazon","IBM");
+        bing_questions[0]=q;
+
+        q=new Questions(QType.Java,"Who is the pioneer of television and creator of the Twilight Zone, grew up on Bennett Avenue on the West Side?(His house still looks creepy tbh)","David Fincher","Rod Sterling","Alfred Hitchcock","Rod Sterling");
+        bing_questions[0]=q;
+
+        q=new Questions(QType.Java," Who is the person that city is named after him? (Never visited Binghamton, but instead was simply a land speculator)","Bruce Wayne","Patrick Madden","William Bingham","William Bingham");
+        bing_questions[0]=q;
+
+        q=new Questions(QType.Java,"What is the default value of byte variable?","0","0.0","null","0");
+        bing_questions[0]=q;
+
+        q=new Questions(QType.Java,"What is the default value of byte variable?","0","0.0","null","0");
+        bing_questions[0]=q;
+
+        q=new Questions(QType.Java,"What is the default value of byte variable?","0","0.0","null","0");
+        bing_questions[0]=q;
+
+        q=new Questions(QType.Java,"What is the default value of byte variable?","0","0.0","null","0");
+        bing_questions[0]=q;
+
+        q=new Questions(QType.Java,"What is the default value of byte variable?","0","0.0","null","0");
+        bing_questions[0]=q;
+
+        q=new Questions(QType.Java,"What is the default value of byte variable?","0","0.0","null","0");
+        bing_questions[0]=q;
+
+        q=new Questions(QType.Java,"What is the default value of byte variable?","0","0.0","null","0");
+        bing_questions[0]=q;
 
     }
 
